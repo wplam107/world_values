a87f72c189f0713fc0ac6346a42cb0cd5ed25920
Wayne <w.p.lam107@gmail.com>
2020-11-25 21:31:38 -0500

    updates and rename

---
+++ functions/graph_functions.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import plotly.graph_objects as go
+
+TRAITS = [
+    'manners',
+    'independence',
+    'hard work',
+    'responsibility',
+    'imagination',
+    'tolerance',
+    'thrift',
+    'determination',
+    'faith',
+    'unselfishness',
+    'obedience'
+]
+
